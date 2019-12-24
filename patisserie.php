f7347162b3b2ba2479e935915f841f193d99978c
Luc Frébourg <l.frebourg@gmail.com>
2019-12-24 16:29:32 +0100

    Update patisserie.php

--- patisserie.php
+++ patisserie.php
@@ -15,7 +15,7 @@
 		$vcap_services = json_decode($_ENV['VCAP_SERVICES'], true);
 		$uri = $vcap_services['compose-for-mysql'][0]['credentials']['uri'];
 		$db_creds = parse_url($uri);
-		$dbname = "patisserie";
+		$dbname = "compose";
 
 		$dsn = "mysql:host=" . $db_creds['host'] . ";port=" . $db_creds['port'] . ";dbname=" . $dbname;
 		$dbh = new PDO($dsn, $db_creds['user'], $db_creds['pass'],array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
